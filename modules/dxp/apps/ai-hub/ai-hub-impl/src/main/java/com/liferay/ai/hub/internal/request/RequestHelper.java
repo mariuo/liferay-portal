54a357fe1b0daee815ba95e276e03974135b0a70
Tina Tian <tina.tian@liferay.com>
2026-06-01 18:01:35 -0700

    LPD-93148 Reset AI Hub request locks on master node startup

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/request/RequestHelper.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/request/RequestHelper.java
@@ -5,8 +5,19 @@
 
 package com.liferay.ai.hub.internal.request;
 
+import com.liferay.portal.kernel.cluster.ClusterExecutor;
+import com.liferay.portal.kernel.cluster.ClusterMasterExecutor;
+import com.liferay.portal.kernel.cluster.ClusterNode;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.lock.model.Lock;
+import com.liferay.portal.lock.service.LockLocalService;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Tina Tian
@@ -17,9 +28,46 @@ import org.osgi.service.component.annotations.Modified;
 )
 public class RequestHelper {
 
+	@Activate
+	protected void activate() {
+		if (!_clusterMasterExecutor.isMaster()) {
+			return;
+		}
+
+		if (_clusterExecutor.isEnabled()) {
+			List<ClusterNode> clusterNodes = _clusterExecutor.getClusterNodes();
+
+			if (clusterNodes.size() > 1) {
+				return;
+			}
+		}
+
+		_companyLocalService.forEachCompany(
+			company -> {
+				List<Lock> locks = _lockLocalService.getLocks(
+					company.getCompanyId(), RequestUtil.class.getName());
+
+				for (Lock lock : locks) {
+					_lockLocalService.deleteLock(lock);
+				}
+			});
+	}
+
 	@Modified
 	protected void modified() {
 		RequestUtil.reset();
 	}
+
+	@Reference
+	private ClusterExecutor _clusterExecutor;
+
+	@Reference
+	private ClusterMasterExecutor _clusterMasterExecutor;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private LockLocalService _lockLocalService;
 
 }
